15cb4c3b1c80bc9880b72685b458b3a918c76477
ObadaAbuhajjeh <obada.abuhajjeh.1@gmail.com>
2024-09-07 03:50:00 +0300

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function Contact() {
+  const [formInputs, setFormInputs] = useState({
+    name: '',
+    email: '',
+    age: '',
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormInputs({
+      ...formInputs,
+      [name]: value,
+    })
+  }
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log(formInputs)
+  }
+  return (
+    <>
+      <form onSubmit={submitForm}>
+        <label>User Name </label>
+        <input type="text" value={formInputs.name} name='name' onChange={handleChange} />
+
+        <label>Email </label>
+        <input type="email" value={formInputs.email} name='email' onChange={handleChange} />
+        <label>Age </label>
+        <input type="text" value={formInputs.age} name='age' onChange={handleChange} />
+
+        <hr />
+        <button type='submit'>Submit</button>
+      </form>
+    </>
+  )
+}
---
+++ src/components/Player.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+export default function Player() {
+  const [player, setPlayer] = useState({
+    firstName: 'Ranjani',
+    lastName: 'Shettar',
+  });
+  const [score, setScore] = useState(10);
+
+  function handlePlusClick() {
+    setScore(score + 1);
+  }
+
+  function handleFirstNameChange(e) {
+    setPlayer({
+      ...player,
+      firstName: e.target.value,
+    });
+  }
+
+  function handleLastNameChange(e) {
+    setPlayer({
+      ...player,
+      lastName: e.target.value
+    });
+  }
+
+  return (
+    <>
+      <label>
+        Score: <b>{score}</b>
+        {' '}
+        <button onClick={handlePlusClick}>
+          +1
+        </button>
+      </label>
+      <label>
+        First name:
+        <input
+          value={player.firstName}
+          onChange={handleFirstNameChange}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          value={player.lastName}
+          onChange={handleLastNameChange}
+        />
+      </label>
+    </>
+  );
+}
